f630630ff202961666a325dca72f5b5530621cab
matkateresa <attwistingnether@gmail.com>
2019-02-16 23:17:13 +0100

    .

---
+++ Candy/wwwroot/precache-manifest.c7182fedd1fe69e3de247263976c417f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "d4f25e666ccb96681c44",
+    "url": "/static/js/main.d4f25e66.chunk.js"
+  },
+  {
+    "revision": "00f7601466d1f70047ca",
+    "url": "/static/js/1.00f76014.chunk.js"
+  },
+  {
+    "revision": "d4f25e666ccb96681c44",
+    "url": "/static/css/main.3c636b3e.chunk.css"
+  },
+  {
+    "revision": "af4a193c5b57455443617532896a0f66",
+    "url": "/index.html"
+  }
+];
