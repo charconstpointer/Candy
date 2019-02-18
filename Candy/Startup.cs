cabb5ba3a572a54679badcaff029030269b00d66
matkateresa <attwistingnether@gmail.com>
2019-02-18 09:56:12 +0100

    red

--- Candy/Startup.cs
+++ Candy/Startup.cs
@@ -1,10 +1,8 @@
-﻿using System.IO;
-using Candy.Hubs;
+﻿using Candy.Hubs;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.FileProviders;
 
 namespace Candy
 {
